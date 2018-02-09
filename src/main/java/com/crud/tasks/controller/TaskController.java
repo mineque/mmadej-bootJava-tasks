d06fa2c004aad2f7e5648439faa77b070152fb46
mineque <mineque@quanteam.pl>
2018-02-09 23:03:23 +0100

    Mod15.2 EX

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,29 +1,35 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("v1/task")
 public class TaskController {
 
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
-//        List<TaskDto> tasks = new ArrayList<>();
-//        tasks.add(new TaskDto((long)1,"Title1","Content 1"));
-//        tasks.add(new TaskDto((long)1,"Title2","Content 2"));
-//        tasks.add(new TaskDto((long)1,"Title3","Content 3"));
-//        tasks.add(new TaskDto((long)1,"Title4","Content 4"));
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(String taskId){
-        return new TaskDto((long)1,"test title","conent");
+    public Optional<Task> getTask(Long id){
+        return service.getTaskById(id);
     }
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public String deleteTask(String taskId) {
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -15,5 +16,15 @@ public class DbService {
     public List<Task> getAllTasks(){
         return repository.findAll();
     }
-
+    public Optional<Task> getTaskById(Long id){
+        return repository.findById(id);
+    }
 }
+
+
+
+/*Utwórz nową metodę w serwisie bazy danych - DbService, służącą do pobierania zadań przy użyciu id zadania.
+Jeśli będziesz miał problemy z wykonaniem zadania, spróbuj najpierw poszukać informacji na własną rękę -
+to nieodłączny element nauki oraz pracy jako programista :)
+ */
+
